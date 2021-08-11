5a0e12c8ee2d171a978cea8ddc873b566a6061fc
jeadka1 <jeadka1@gmail.com>
2021-08-11 15:41:59 +0900

    2021-0811_prev_test

--- src/main_command_node.cpp
+++ src/main_command_node.cpp
@@ -2,18 +2,20 @@
 #include "nodelet/nodelet.h"
 #include <pluginlib/class_list_macros.h>
 
+#include <std_msgs/Bool.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/Float32MultiArray.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <signal.h>
 
 namespace auto_driving {
 
-class MainNode : public nodelet::Nodelet {
+class MaincommandNode : public nodelet::Nodelet {
 
 public:
-	MainNode() = default;
+	MaincommandNode() = default;
 
 private:
 	virtual void onInit() {
@@ -23,18 +25,23 @@ private:
 		// Configuration //
 		nhp.param("obstacle_coefficient", config_.obstacle_coefficient_, 0.005);
 		nhp.param("front_obstacle_dist", config_.front_obstacle_dist_, 0.1);
+//    sub_qr_ = nhp.subscribe<std_msgs::Bool> ("/lidar_driving", 10, &CmdPublishNode::publishCmd, this);
+		sub_start_ = nhp.subscribe<std_msgs::Bool> ("/main_flag", 10, &MaincommandNode::MainCallback, this);
+		sub_kill_ = nhp.subscribe<std_msgs::Bool> ("/kill_flag", 10, &MaincommandNode::killCallback, this);
 
-		sub_qr_ = nhp.subscribe("/main_flag", 10, &MainNode::qrCallback, this);
-
-   	sub_pub_cmd_ = nhp.advertise<std_msgs::Float32MultiArray> ("/qr_cmd", 10);
+   	sub_pub_cmd_ = nhp.advertise<std_msgs::Float32MultiArray> ("/main_test", 10);
 	};
 
 private:
-	ros::Subscriber sub_qr_;
+	ros::Subscriber sub_start_;
+	ros::Subscriber sub_kill_;
 	ros::Publisher sub_pub_cmd_;
 
+	int pid_id[10];	
 
-	void qrCallback(const std_msgs::Float32MultiArray::ConstPtr& trans_msg);
+	bool start_up=false;
+	void MainCallback(const std_msgs::Bool::ConstPtr &main_msg);
+	void killCallback(const std_msgs::Bool::ConstPtr &kill_msg);
 
 
 	/** configuration parameters */
@@ -45,18 +52,43 @@ private:
 	} Config;
 	Config config_;
 };
-void MainNode::qrCallback(const std_msgs::Float32MultiArray::ConstPtr& trans_msg)
+void MaincommandNode::killCallback(const std_msgs::Bool::ConstPtr &kill_msg)
+{
+
+	//pid = fork();
+  kill(pid_id[0], SIGKILL);
+  printf("killed process group %d\n", pid_id[0]);
+}
+void MaincommandNode::MainCallback(const std_msgs::Bool::ConstPtr &main_msg)
 {
 	std_msgs::Float32MultiArray data_test;
 
 	data_test.data.clear();
-	data_test.data.push_back(trans_msg->data[0]); // global_dist_err
-	data_test.data.push_back(trans_msg->data[1]); // global_dist_err
-
-	system("ls");
+	//data_test.data.push_back(trans_msg); //
+	data_test.data.push_back(100); //
+	if(!start_up)
+	{
+		pid_t pid;
+		pid = fork();
+		if(pid>0)
+		{
+			
+		}
+		else if(pid==0)
+		{
+			pid_id[0] = getpid();
+			std::cout<<"child process: "<<pid_id[0]<<std::endl;
+			system("cd ~/catkin_ws; roslaunch roverrobotics_driver zero2_teleop.launch");
+			start_up = true;
+		}
+		else if (pid ==-1)
+		{
+			std::cout<<"error"<<std::endl;
+		}
+	}
 	sub_pub_cmd_.publish(data_test);
 }
 }
 
-PLUGINLIB_EXPORT_CLASS(auto_driving::MainNode, nodelet::Nodelet);
+PLUGINLIB_EXPORT_CLASS(auto_driving::MaincommandNode, nodelet::Nodelet);
 
